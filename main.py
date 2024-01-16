b9276c4829271b05306c7a69e5b29178b2456827
tct123 <42028373+tct123@users.noreply.github.com>
2024-01-16 03:59:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,30 @@ def main(page: ft.Page):
     page.title = "Portfolio"
     page.appbar = ft.AppBar(title=ft.Text(page.title))
     text = ft.Text("Hello, Flet!")
-    textfield = ft.TextField()
+    textfield = ft.TextField(hint_text="enter text")
+
     def change(e):
         text.value = str(textfield.value)
-        page.update()
+        if text.value == "":
+            pass
+        else:
+            page.update()
+
     page.add(
         ft.SafeArea(
             ft.Row(
                 controls=[
                     text,
-                    ft.TextButton(text="Change Text", on_click=change)
                 ]
-            ),
-            ft.Row(controls=[ft.Text("Next")])
+            )
         )
     )
     page.add(ft.SafeArea(ft.Row(controls=[textfield])))
-
-    
+    page.add(
+        ft.SafeArea(
+            ft.TextButton(text="Change Text", on_click=change)
+        )
+    )
 
 
 ft.app(main)
