729153af7f9dc837e2179ff4cb51879f7f5142ea
tct123 <42028373+tct123@users.noreply.github.com>
2025-02-15 15:32:44 +0100

    Revert "."
    This reverts commit 132ac053d1880fa37c12091b6d4458caa08642e6.

--- src/infos/__init__.py
+++ src/infos/__init__.py
@@ -1,16 +1,31 @@
 import flet as ft
 
 
-def myappbar(title, bgcolor, items: list):
+def myappbar(page):
     return ft.AppBar(
-        title=ft.Text(title),
+        title=ft.Text(page.title),
         center_title=True,
         adaptive=True,
-        bgcolor=bgcolor,
+        bgcolor=page.bgcolor,
         actions=[
             ft.PopupMenuButton(
-                bgcolor=bgcolor,
-                items=items,
+                bgcolor=page.bgcolor,
+                items=[
+                    ft.PopupMenuItem(
+                        icon=ft.Icons.CLOUD,
+                        text="Github",
+                        on_click=lambda _: page.launch_url(
+                            url="https://github.com/tct123"
+                        ),
+                    ),
+                    ft.PopupMenuItem(
+                        icon=ft.Icons.PLAY_CIRCLE,
+                        text="YouTube",
+                        on_click=lambda _: page.launch_url(
+                            url="https://youtube.com/@tc-diy"
+                        ),
+                    ),
+                ],
             )
         ],
     )
