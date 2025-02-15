cb82e557bb6db7949ddb4d0ae6677aa98cffd115
tct123 <42028373+tct123@users.noreply.github.com>
2025-02-15 15:58:17 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,28 +1,43 @@
 import flet as ft
-import colors as cs
-import os
 from infos import myappbar
 from background import mydecoration
 
 
 def main(page: ft.Page):
-    bgcolor = ft.Colors.TRANSPARENT
     page.title = "Flet Portfolio"
     page.decoration = mydecoration()
-    page.adaptive = True
-    page.bgcolor = bgcolor
-    page.scroll = True
-    page.appbar = myappbar(page=page)
-    page.bottom_appbar = ft.BottomAppBar(
-        bgcolor=bgcolor,
-        content=ft.Row(
-            controls=[
-                ft.IconButton(icon=ft.Icons.MENU, icon_color=ft.Colors.WHITE),
-                ft.Container(expand=True),
-            ]
-        ),
-    )
-    page.add(ft.SafeArea(ft.Container()))
+    def route_change(e):
+        page.views.clear()
+
+        if page.route == "/":
+            page.views.append(
+                ft.View(
+                    route="/",
+                    appbar=myappbar(page=page),
+                    controls=[
+                        ft.Text("Home Seite"),
+                        ft.ElevatedButton(
+                            "Gehe zu /about", on_click=lambda _: page.go("/about")
+                        ),
+                    ],
+                )
+            )
+        elif page.route == "/about":
+            page.views.append(
+                ft.View(
+                    route="/about",
+                    appbar=myappbar(page=page),
+                    controls=[
+                        ft.Text("Über uns"),
+                        ft.ElevatedButton("Zurück", on_click=lambda _: page.go("/")),
+                    ],
+                )
+            )
+
+        page.update()
+
+    page.on_route_change = route_change
+    page.go(page.route)
 
 
 ft.app(target=main)
