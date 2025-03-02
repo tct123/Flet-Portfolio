47e5f37fe0315c15469e9fa85f0e9f7e8bb2a288
tct123 <42028373+tct123@users.noreply.github.com>
2025-03-02 02:10:37 +0100

    Update translations.py

--- src/localisation/translations.py
+++ src/localisation/translations.py
@@ -6,8 +6,8 @@ import flet as ft
 
 myplatfom = platform.system().lower()
 trfile = f"{os.path.dirname(__file__)}/localisation.csv"
-langcode=locale(platform=myplatfom).split("_")[0]
-print(langcode)
+langcode = locale(platform=myplatfom).split("_")[0]
+region = locale(platform=myplatfom).split("_")[1]
 HELLOMSG = tr(
     csv_file=trfile,
     target_key="HELLOMSG",
