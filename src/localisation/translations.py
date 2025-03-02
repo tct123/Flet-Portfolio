bc3fef124842ee2a16784b3a3bb37cfefd48a6d3
tct123 <42028373+tct123@users.noreply.github.com>
2025-03-02 02:37:32 +0100

    .

--- src/localisation/translations.py
+++ src/localisation/translations.py
@@ -2,19 +2,28 @@ import os
 import platform
 from mylocale import tr
 from flet_localisation import locale
+import flet as ft
 
-myplatfom = platform.system().lower()
 trfile = f"{os.path.dirname(__file__)}/localisation.csv"
-langcode = str(locale(platform=myplatfom)).split("_")[0]
-# region = str(locale(platform=myplatfom)).split("_")[1]
 
-HELLOMSG = tr(
-    csv_file=trfile,
-    target_key="HELLOMSG",
-    langcode=langcode,
-)
-CONTNUEBTN = tr(
-    csv_file=trfile,
-    target_key="CONTNUEBTN",
-    langcode=langcode,
-)
+
+def HELLOMSG(page: ft.Page):
+    myplatfom = str(page.platform)
+    langcode = str(locale(platform=myplatfom)).split("_")[0]
+    region = str(locale(platform=myplatfom)).split("_")[1]
+    return tr(
+        csv_file=trfile,
+        target_key="HELLOMSG",
+        langcode=langcode,
+    )
+
+
+def CONTNUEBTN(page: ft.Page):
+    myplatfom = str(page.platform)
+    langcode = str(locale(platform=myplatfom)).split("_")[0]
+    region = str(locale(platform=myplatfom)).split("_")[1]
+    return tr(
+        csv_file=trfile,
+        target_key="CONTNUEBTN",
+        langcode=langcode,
+    )
